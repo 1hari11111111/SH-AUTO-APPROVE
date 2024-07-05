6d3ad56d4271c8e83008b4031405de8b42c121e5
1hari11111111 <uhvardhan987@gmail.com>
2024-07-05 16:22:19 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -6,12 +6,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "28525384"))
-    API_HASH = getenv("API_HASH", "3a1190585fe5bf1f6324be87ba5b68c6")
+    API_ID = int(getenv("API_ID", "17822592"))
+    API_HASH = getenv("API_HASH", "a20b3dbbe07ed695563b4609a3e62012")
     BOT_TOKEN = getenv("BOT_TOKEN", "0")
-    FSUB = getenv("FSUB", "Straw_Hat_Bots")
+    FSUB = getenv("FSUB", "mtpmasala")
     CHID = int(getenv("CHID", "-1002059817947"))
-    SUDO = list(map(int, getenv("SUDO", "6727550037 7162615398").split()))
+    SUDO = list(map(int, getenv("SUDO", "578811855").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sagatobots00001:sagatobots100@cluster00001.vgdshkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001")
     
 cfg = Config()
